c5832708c06c57acc488839c475990a14a162d39
SolRomaniuk <masolromaniuk@gmail.com>
2024-06-23 19:55:35 -0300

    Cambios en db.js con datos host, user,password,database"

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,10 +1,15 @@
 import mysql from 'mysql2' /* LOS MODULOS DE TERCEROS NO LLEVAN LA EXTESION .js */
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "pasterie_integrador",
+  //host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "pasterie_integrador",
+  host: "mysql-solromaniuk86.alwaysdata.net",
+  user: "359679_pasterie",
+  password: "Comision24130",
+  database: "solromaniuk86_pasterie_integrador",
+
 });
 
 db.connect((error) => {
